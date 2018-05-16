3c31bbc49d17a9222c540aae34412f4659959d8c
mintaka33 <mintaka33@outlook.com>
2018-05-16 15:14:58 -0700

    use opencv verison 4.0.0 (private build)

--- opencv_caffe/object_detection/object_detection.cpp
+++ opencv_caffe/object_detection/object_detection.cpp
@@ -8,6 +8,8 @@
 #include <opencv2/dnn/shape_utils.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/videoio/videoio_c.h>
+
 #include <fstream>
 #include <iostream>
 
