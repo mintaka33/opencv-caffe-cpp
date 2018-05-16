4e66d467d8462c33b27aceffd8cb635569fac391
mintaka33 <mintaka33@outlook.com>
2018-05-16 16:45:30 -0700

    DNNDetetor class basic implementation

--- opencv_caffe/object_detection_class/detector.h
+++ opencv_caffe/object_detection_class/detector.h
@@ -1,13 +1,40 @@
 #pragma once
 #include "stdafx.h"
 
+#include <fstream>
+#include <sstream>
+
+#include <opencv2/dnn/dnn.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+using namespace cv;
+using namespace dnn;
+
 class DNNDetector
 {
 public:
     DNNDetector();
     ~DNNDetector();
 
-private:
+    int initNet(String cfgFile, String modelFile, String framework);
+    int detectFrame(Mat* inputFrame);
 
+private:
+    std::vector<String> DNNDetector::getOutputsNames(const Net& net);
+    void DNNDetector::postprocess(Mat& frame, const std::vector<Mat>& outs, Net& net);
+
+private:
+    float confThreshold = 0.5; // Confidence threshold
+    float scale = (float)0.00392; // (1/255)
+    Scalar mean = {0.0, 0.0, 0.0, 0.0};
+    bool swapRB = false;
+    int inpWidth = 300;
+    int inpHeight = 300;
+    int backend = 0; // 0: default C++ backend; 1: Halide language; 2: Intel CVSDK Inference Engine
+    int target = 0;  // 0: CPU target (by default); 1: OpenCL"
+
+    std::vector<std::string> classes;
+    Net net;
 };
 
--- opencv_caffe/object_detection_class/object_detection_class.cpp
+++ opencv_caffe/object_detection_class/object_detection_class.cpp
@@ -2,10 +2,22 @@
 //
 
 #include "stdafx.h"
-
+#include "detector.h"
 
 int main()
 {
+    String cfgFile = "MobileNetSSD_deploy.prototxt.txt";
+    String modelFile = "MobileNetSSD_deploy.caffemodel";
+
+    DNNDetector detector;
+    detector.initNet(cfgFile, modelFile, "caffe");
+
+    Mat frame;
+    for (int i=0; i<1; i++)
+    {
+        detector.detectFrame(&frame);
+    }
+
     return 0;
 }
 
